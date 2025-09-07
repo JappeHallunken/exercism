d17aaee4900b61da8788fe5c4d422c0c1ee2f4af
Paul Nürnberger <jappe@posteo.de>
2025-09-08 01:06:54 +0200

    Init commit

---
+++ go/sorting-room/sorting_room.go
@@ -0,0 +1,61 @@
+package sorting
+
+import (
+	"fmt"
+	"strconv"
+)
+
+// DescribeNumber should return a string describing the number.
+func DescribeNumber(f float64) string {
+	return fmt.Sprintf("This is the number %.1f", f)
+}
+
+type NumberBox interface {
+	Number() int
+}
+
+// DescribeNumberBox should return a string describing the NumberBox.
+func DescribeNumberBox(nb NumberBox) string {
+ return fmt.Sprintf("This is a box containing the number %.1f", float64(nb.Number()))
+}
+
+type FancyNumber struct {
+	n string
+}
+
+func (i FancyNumber) Value() string {
+	return i.n
+}
+
+type FancyNumberBox interface {
+	Value() string
+}
+
+// ExtractFancyNumber should return the integer value for a FancyNumber
+// and 0 if any other FancyNumberBox is supplied.
+func ExtractFancyNumber(fnb FancyNumberBox) int {
+	if str, ok := fnb.(FancyNumber); ok {
+		val, err := strconv.Atoi(str.Value())
+		if err != nil {
+			return 0
+		}
+		return val
+	}
+	return 0
+}
+
+// DescribeFancyNumberBox should return a string describing the FancyNumberBox.
+func DescribeFancyNumberBox(fnb FancyNumberBox) string {
+	nb := 0.0
+	if str, ok := fnb.(FancyNumber); ok {
+		val, err := strconv.ParseFloat(str.Value(), 64)
+		if err == nil {
+			nb = val
+		}
+ }
+ return fmt.Sprintf("This is a fancy box containing the number %.1f", nb)}
+
+// DescribeAnything should return a string describing whatever it contains.
+func DescribeAnything(i interface{}) string {
+	panic("Please implement DescribeAnything")
+}
