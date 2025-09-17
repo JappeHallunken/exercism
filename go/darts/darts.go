3c439c949c55eaf1beaae0a238e0715c567275cd
Paul Nürnberger <jappe@posteo.de>
2025-09-17 15:59:39 +0200

    darts

---
+++ go/darts/darts.go
@@ -0,0 +1,19 @@
+package darts
+
+import "math"
+
+const outerR = 10.0
+const midR = 5.0
+const innerR = 1
+
+const outerC = 1
+const midC = 5
+const innerC = 10
+
+func Score(x, y float64) int {
+
+}
+
+getDistance(x,y float64) float64 {
+	math.
+}
